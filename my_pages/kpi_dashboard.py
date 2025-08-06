a0872dc1752817b4ef45cc72b2130d906c48161f
enomcdcdash <analisatorenom@gmail.com>
2025-08-06 11:30:04 +0700

    Update kpi_dashboard.py

--- my_pages/kpi_dashboard.py
+++ my_pages/kpi_dashboard.py
@@ -183,7 +183,7 @@ def app_tab1():
         left_align_css = ", ".join([f'td.{idx}' for idx in left_align_indexes])
 
         html_code = f"""
-        <div width:100%; padding: 10px;">
+        <div style="width:100%; padding: 10px;">
             <style>
                 table {{
                     width: 100% !important;
@@ -278,7 +278,7 @@ def app_tab1():
         # Final HTML with professional styles
         # Final HTML with professional styles
         html_code = f"""
-        <div width:100%; padding: 10px;">
+        <div style="width:100%; padding: 10px;">
             <style>
                 table {{
                     width: 100% !important;
@@ -310,9 +310,9 @@ def app_tab1():
             {table_html}
         </div>
         """
-
+        st.markdown(html_code_wrapped, unsafe_allow_html=True)
         # Render in Streamlit
-        components.html(html_code, height=700, scrolling=True)
+        # components.html(html_code, height=700, scrolling=True)
     
     # --- Summary Score Cards for AREA 1 & 3 Combined ---
     st.markdown(f"### 📋 Average Scores Summary — {month} {year}")
@@ -507,4 +507,5 @@ def app():
         app_tab1()
     with tab2:
         app_tab2()
+
 
